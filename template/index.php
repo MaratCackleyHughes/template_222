ca3930c9dfce8b7a3cbcd47086dd3ec4bf417a13
Marat Cackley-Hughes <59293626+MaratCackleyHughes@users.noreply.github.com>
2020-02-17 11:44:06 -0800

    Add files via upload

--- template/index.php
+++ template/index.php
@@ -24,34 +24,20 @@
 </head>
 
 <body>
-		<?php include 'includes/header.php';?>
+	<?php include 'includes/header.php';?>
 
 	<div class="container">
 		<h1>Sed maximus rhoncus tellus sed interdum.
 		</h1>
 		<p>Ut nulla eros, interdum vel tempus vel, tincidunt at mi. In pretium ligula vitae eros sollicitudin, sed tincidunt arcu tempus. Vivamus egestas tellus at libero aliquet, eget pulvinar dui iaculis. Nam finibus felis magna, non consectetur elit gravida ac. Vivamus ornare libero quis tristique mattis. Aenean ullamcorper sed est eu hendrerit. Aenean pretium justo ut quam mattis, quis suscipit metus lacinia. Donec eleifend interdum dui in efficitur. Fusce imperdiet mollis lacus et egestas. Integer congue bibendum diam in elementum.
 		</p>
-		<div class="well">
-			<form action="/action_page.php">
-				<fieldset>
-					<legend> Sign up now:</legend>
-					First name:<br>
-					<input type="text" name="firstname" value="*"><br>
-					Last name:<br>
-					<input type="text" name="lastname" value="*"><br><br>
-					Email:<br>
-					<input type="email" name="email" value="*"><br><br>
-					Choose an event:<br>
-					<input type="radio" name="eventone" value="male" checked> Male<br>
-					<input type="radio" name="eventtwo" value="female"> Female<br>
-					<input type="radio" name="eventthree" value="other"> Other<br><br>
-					Tell us about yourself:<br>
-					<textarea name="message">write more here...</textarea><br>
-					<button type="submit">Submit</button>
-					<!-- <input type="submit" value="Submit"> -->
-				</fieldset>
-			</form>
-		</div><!-- closes the well class -->
+
+		<p>Ut nulla eros, interdum vel tempus vel, tincidunt at mi. In pretium ligula vitae eros sollicitudin, sed tincidunt arcu tempus. Vivamus egestas tellus at libero aliquet, eget pulvinar dui iaculis. Nam finibus felis magna, non consectetur elit gravida ac. Vivamus ornare libero quis tristique mattis. Aenean ullamcorper sed est eu hendrerit. Aenean pretium justo ut quam mattis, quis suscipit metus lacinia. Donec eleifend interdum dui in efficitur. Fusce imperdiet mollis lacus et egestas. Integer congue bibendum diam in elementum.
+		</p>
+
+		<p>Ut nulla eros, interdum vel tempus vel, tincidunt at mi. In pretium ligula vitae eros sollicitudin, sed tincidunt arcu tempus. Vivamus egestas tellus at libero aliquet, eget pulvinar dui iaculis. Nam finibus felis magna, non consectetur elit gravida ac. Vivamus ornare libero quis tristique mattis. Aenean ullamcorper sed est eu hendrerit. Aenean pretium justo ut quam mattis, quis suscipit metus lacinia. Donec eleifend interdum dui in efficitur. Fusce imperdiet mollis lacus et egestas. Integer congue bibendum diam in elementum.
+		</p>
+
 	</div><!-- closes the container class -->
 	<?php include 'includes/footer.php';?>
 	<script src="js/test1.js"></script>
